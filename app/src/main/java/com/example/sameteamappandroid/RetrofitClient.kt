41b5ce0aac50f358daea9203386f7520061c7fb8
Bob Nie <ylunie9432@students.ecpi.edu>
2025-05-19 11:42:57 -0400

    Updated UI and connected frontend to Azure backend

--- app/src/main/java/com/example/sameteamappandroid/RetrofitClient.kt
+++ app/src/main/java/com/example/sameteamappandroid/RetrofitClient.kt
@@ -4,7 +4,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://10.0.2.2:5073/api/"
+   // private const val BASE_URL = "http://10.0.2.2:5073/api/"
+    private const val BASE_URL = "https://sameteamapiazure-gfawexgsaph0cvg2.centralus-01.azurewebsites.net/api/"
 
     val instance: ApiService by lazy {
         Retrofit.Builder()
